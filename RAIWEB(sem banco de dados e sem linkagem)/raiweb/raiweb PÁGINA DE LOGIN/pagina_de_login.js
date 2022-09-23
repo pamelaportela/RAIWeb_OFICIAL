cf63353a601f1c2e17ae7b9a96ccc1dcfd07ca15
Phillip241 <106562948+Phillip241@users.noreply.github.com>
2022-09-23 13:24:10 -0300

    Add files via upload

---
+++ RAIWEB(sem banco de dados e sem linkagem)/raiweb/raiweb PÁGINA DE LOGIN/pagina_de_login.js
@@ -0,0 +1,13 @@
+function login()
+{
+nome = document.getElementById('nome_de_usuario')
+senha = document.getElementById('senha')
+
+if( nome.value = 'dudu' && senha.value=='calvo')
+{
+    open('https://www.youtube.com/watch?v=3r3YeEm8RAk')
+}else if( nome.value = 'Silvestre' && senha.value=='pistolaquerendodarstrike'){
+    open('https://www.youtube.com/watch?v=xvFZjo5PgG0')
+    
+}
+}
