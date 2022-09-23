5d9a9a49a1c36f20ca9fdd492aafbbd399c6f23b
Phillip241 <106562948+Phillip241@users.noreply.github.com>
2022-09-23 20:37:25 -0300

    Update adicionar_perfil.js

--- RAIWEB(sem banco de dados e sem linkagem)/raiweb/raiweb PAGINA ADICIONAR PERFIL/adicionar_perfil.js
+++ RAIWEB(sem banco de dados e sem linkagem)/raiweb/raiweb PAGINA ADICIONAR PERFIL/adicionar_perfil.js
@@ -1,4 +1,7 @@
-
+function remover_conteudo(id){
+    elemento = document.getElementById(id)
+    elemento.value = ''
+}
 //Funcao geradora de senhas    
 function gerar_senha() {
 
@@ -22,3 +25,34 @@ function comparar_senhas(){
 function adicionar_foto_de_perfil(){
     return(0)
     }
+
+//MASCARA PARA CPF e invalidacao
+input = document.getElementById('cpf')
+//cpf invalido
+input.addEventListener('keypress',() =>{
+    let quantidade_caracteres_input = input.value.length
+    if( quantidade_caracteres_input < 14){
+        input.style = "border:1px solid #ff0000"
+    }else{
+        input.style = "border:1px solid ##05053F"
+    }
+})
+
+function validacao_cpf(){
+    if(input.value.length <14){
+        alert('CPF invalido')
+    }
+}
+//mascara
+input.addEventListener('keypress',() =>{
+    let quantidade_caracteres_input = input.value.length
+    if(quantidade_caracteres_input === 3 || quantidade_caracteres_input === 7){
+        input.value += '.'
+    }else if(quantidade_caracteres_input === 11){
+        input.value += '-'
+    }
+})
+
+var filtro_teclas = function(event) {
+    return ((event.charCode >= 48 && event.charCode <= 57) || (event.keyCode == 45 || event.charCode == 46))
+  }
