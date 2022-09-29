f1da477f9abc97d6718c36c4901c69a8aa132d78
Phillip241 <106562948+Phillip241@users.noreply.github.com>
2022-09-29 14:08:28 -0300

    Update pagina_de_login.js

--- RAIWEB(sem banco de dados e sem linkagem)/raiweb/raiweb PÁGINA DE LOGIN/pagina_de_login.js
+++ RAIWEB(sem banco de dados e sem linkagem)/raiweb/raiweb PÁGINA DE LOGIN/pagina_de_login.js
@@ -1,10 +1,15 @@
 function login()
 {
+// Declaracao de variavel
 nome = document.getElementById('nome_de_usuario')
 senha = document.getElementById('senha')
+// getElementsByTagName retorna um vetor
 botao = document.getElementsByTagName('button')
+// Salvando os dados antigos inseridos nos input
 verificacao_nome = nome.value;
 verificacao_senha = senha.value;
+
+//
 if( nome.value = 'dudu' && senha.value=='gente boa')
 {
     open('https://www.youtube.com/watch?v=3r3YeEm8RAk')
@@ -19,6 +24,7 @@ else if( nome.value = 'Silvestre' && senha.value=='pistolaquerendodarstrike'){
 
 }
 
+// Quando os dados inseridos nao conferem
 else{
    let inputs = document.getElementsByTagName('input')
    nome.value = verificacao_nome
@@ -29,7 +35,7 @@ else{
         botao[0].style = 'border: 2px solid red' 
     }
 
-  
+//   setTimeout faz com que a funcao dentro das chaves so rode apos o tempo(milisegundos) indicados apos a virgula
     setTimeout(() => { 
 
     for(i=0; i< inputs.length; i++)
@@ -43,4 +49,22 @@ else{
     
 }
 
+}
+
+function problema(){
+    var elemento_pai = document.getElementById('corpo')
+    var elemento_novo = document.createElement('iframe')
+    var elemento_ambaixo = document.getElementById
+    ('elemento_a_frente');
+
+    elemento_novo.setAttribute('id','melhor_miranha')
+    elemento_novo.setAttribute('src','../imagens_gerais/WhatsApp Video 2022-09-24 at 11.57.19.mp4')
+    elemento_novo.setAttribute('width','80%')
+    elemento_novo.setAttribute('height','80%')
+
+    elemento_pai.insertBefore(elemento_novo,elemento_ambaixo);
+    setTimeout(() => { window.location.href='#foo';}, 100);
+
+    var elemento_apagado = document.getElementById('melhor_miranha')
+    setTimeout(() => { elemento_apagado.parentNode.removeChild(elemento_apagado);}, 4000);
 }
