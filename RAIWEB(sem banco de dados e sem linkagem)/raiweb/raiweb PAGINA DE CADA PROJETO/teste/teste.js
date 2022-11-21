07caad659f596ce137d33244bb6947cf872764a1
Phillip241 <106562948+Phillip241@users.noreply.github.com>
2022-11-21 20:27:02 -0300

    Delete RAIWEB(sem banco de dados e sem linkagem)/raiweb directory

--- RAIWEB(sem banco de dados e sem linkagem)/raiweb/raiweb PAGINA DE CADA PROJETO/teste/teste.js
+++
@@ -1,8 +0,0 @@
-const box = document.getElementById('box');
-
-const allChildren = document.getElementsByTagName('div').length;
-
-/*const directChildren = box.children.length;
-console.log(directChildren); */
-
-prompt(allChildren)
