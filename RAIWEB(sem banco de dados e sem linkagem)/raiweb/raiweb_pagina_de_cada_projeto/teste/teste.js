3fc57b8f100f3f25a83e9bf926a3e7d5500a3574
Phillip241 <106562948+Phillip241@users.noreply.github.com>
2022-11-21 20:28:00 -0300

    Add files via upload

---
+++ RAIWEB(sem banco de dados e sem linkagem)/raiweb/raiweb_pagina_de_cada_projeto/teste/teste.js
@@ -0,0 +1,8 @@
+const box = document.getElementById('box');
+
+const allChildren = document.getElementsByTagName('div').length;
+
+/*const directChildren = box.children.length;
+console.log(directChildren); */
+
+prompt(allChildren)
